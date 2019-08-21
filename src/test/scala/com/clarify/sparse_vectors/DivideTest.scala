7b2cbe335f0bc698ce0e9015794f82b115b43070
Imran Qureshi <imranq2@hotmail.com>
2019-08-21 11:39:39 -0700

    add more
    Signed-off-by: Imran Qureshi <imranq2@hotmail.com>

--- src/test/scala/com/clarify/sparse_vectors/DivideTest.scala
+++ src/test/scala/com/clarify/sparse_vectors/DivideTest.scala
@@ -6,54 +6,15 @@ import org.apache.spark.sql.{QueryTest, Row}
 import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector}
 import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 
-class AddTest extends QueryTest with SparkSessionTestWrapper {
+class DivideTest extends QueryTest with SparkSessionTestWrapper {
 
-  test("add simple") {
-    val v1 = new SparseVector(3, Array(0), Array(0.1))
-    val v2 = new SparseVector(3, Array(0), Array(0.1))
-    val v3 = new SparseVectorAdd().sparse_vector_add(v1, v2)
-    assert(v3 == new SparseVector(3, Array(0), Array(0.2)))
+  ignore("divide simple") {
+    val v1 = new SparseVector(3, Array(0), Array(0.6))
+    val v2 = new SparseVector(3, Array(0), Array(0.2))
+    val v3 = new SparseVectorDivide().sparse_vector_divide(v1, v2)
+    assert(v3 == new SparseVector(3, Array(0), Array(0.3)))
   }
-  test("add to itself") {
-    spark.sharedState.cacheManager.clearCache()
-
-    val data = List(
-      Row(new SparseVector(3, Array(0, 2), Array(0.1, 0.2))),
-      Row(new SparseVector(3, Array(0), Array(0.1))),
-      Row(new SparseVector(3, Array(0), Array(0.1)))
-    )
-
-    val fields = List(StructField("input_col", VectorType, nullable = false))
-
-    val data_rdd = spark.sparkContext.makeRDD(data)
-
-    val df = spark.createDataFrame(data_rdd, StructType(fields))
-
-    df.createOrReplaceTempView("my_table2")
-
-    df.show()
-
-    val add_function = new SparseVectorAdd().call _
-
-    spark.udf.register("sparse_vector_add", add_function)
-
-    val out_df = spark.sql(
-      "select sparse_vector_add(input_col, input_col) as result from my_table2"
-    )
-
-    out_df.show()
-    
-    checkAnswer(
-      out_df.selectExpr("result"),
-      Seq(
-        Row(new SparseVector(3, Array(0, 2), Array(0.2, 0.4))),
-        Row(new SparseVector(3, Array(0), Array(0.2))),
-        Row(new SparseVector(3, Array(0), Array(0.2)))
-      )
-    )
-    assert(3 == out_df.count())
-  }
-  test("add to other vectors") {
+  test("divide by other vectors") {
     spark.sharedState.cacheManager.clearCache()
 
     val data = List(
