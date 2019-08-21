3eb883214cf98b34e58cd0a9e108bdee995f42a7
Imran Qureshi <imranq2@hotmail.com>
2019-08-20 18:28:52 -0700

    add GetByIndex
    Signed-off-by: Imran Qureshi <imranq2@hotmail.com>

---
+++ src/main/scala/com/clarify/sparse_vectors/GetByIndex.scala
@@ -0,0 +1,31 @@
+package com.clarify.sparse_vectors
+import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector}
+import scala.collection.mutable
+import org.apache.spark.ml.linalg.Vectors
+import scala.util.control.Breaks._
+import org.apache.spark.sql.api.java.UDF2
+import org.apache.spark.sql.api.java.UDF3
+
+class SparseVectorGetByIndex extends UDF3[SparseVector, Int, Double, Double] {
+
+  override def call(
+      v1: SparseVector,
+      index: Int,
+      default_value: Double
+  ): Double = {
+    sparse_vector_get_float_by_index(v1, index, default_value)
+  }
+
+  def sparse_vector_get_float_by_index(
+      v1: SparseVector,
+      index_to_find: Int,
+      default_value: Double
+  ): Double = {
+    for (i <- 0 until (v1.indices.size)) {
+      val index = v1.indices(i)
+      if (index == index_to_find)
+        return v1.values(i)
+    }
+    return default_value
+  }
+}
