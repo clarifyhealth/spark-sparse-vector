afc5608bd21f4dcc9a87bf6cc268e45e4c2d805e
Imran Qureshi <imranq2@hotmail.com>
2019-09-05 13:09:42 -0700

    add simple sparklistener

--- src/main/scala/com/clarify/sparklistener/ClarifySparkListener.scala
+++ src/main/scala/com/clarify/sparklistener/ClarifySparkListener.scala
@@ -7,17 +7,17 @@ class ClarifySparkListener extends SparkListener {
   private val SPARK_JOB_GROUP_ID = "spark.jobGroup.id"
 
   override def onJobStart(jobStart: SparkListenerJobStart) {
-    println(s"[ClarifySparkListener] Job ${jobStart.jobId} started with ${jobStart.stageInfos.size}")
-    for (stage <- jobStart.stageInfos) {
-      println(s"id=${stage.stageId} name=${stage.name} tasks=${stage.numTasks}")
-    }
     val lastStageInfo = jobStart.stageInfos.sortBy(_.stageId).lastOption
     val jobName = lastStageInfo.map(_.name).getOrElse("")
     val jobGroup = Option(jobStart.properties)
       .flatMap { p => Option(p.getProperty(SPARK_JOB_GROUP_ID)) }
     val jobDescription = Option(jobStart.properties)
       .flatMap { p => Option(p.getProperty(SPARK_JOB_DESCRIPTION)) }
-    println(s"jobName=$jobName description=$jobDescription group=$jobGroup")
+    println(s"[ClarifySparkListener] Job ${jobStart.jobId} jobName=$jobName description=$jobDescription group=$jobGroup"
+      + s" started with ${jobStart.stageInfos.size} tasks:")
+    for (stage <- jobStart.stageInfos) {
+      println(s"[ClarifySparkListener] Task id=${stage.stageId} name=${stage.name} tasks=${stage.numTasks}")
+    }
   }
 
   override def onJobEnd(jobEnd: SparkListenerJobEnd) {
@@ -28,12 +28,13 @@ class ClarifySparkListener extends SparkListener {
     val description = Option(stageSubmitted.properties).flatMap { p =>
       Option(p.getProperty(SPARK_JOB_DESCRIPTION))
     }
-    println(s"[ClarifySparkListener] Stage Submitted ${stageSubmitted.stageInfo.stageId} ${stageSubmitted.stageInfo.name} desc=$description ${stageSubmitted.stageInfo.name}")
+    println(s"[ClarifySparkListener] Stage ${stageSubmitted.stageInfo.stageId} Submitted ${stageSubmitted.stageInfo.name}"
+      + s" desc=$description ${stageSubmitted.stageInfo.name}")
   }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
-    println(s"[ClarifySparkListener] Stage completed ${stageCompleted.stageInfo.stageId} completed with ${stageCompleted.stageInfo.numTasks} tasks.")
-    println(s"[ClarifySparkListener]  Stage completed, runTime: ${stageCompleted.stageInfo.taskMetrics.executorRunTime}, " +
+    println(s"[ClarifySparkListener] Stage completed ${stageCompleted.stageInfo.stageId} completed "
+      + s"with ${stageCompleted.stageInfo.numTasks} tasks. runTime: ${stageCompleted.stageInfo.taskMetrics.executorRunTime}, " +
       s"cpuTime: ${stageCompleted.stageInfo.taskMetrics.executorCpuTime}")
 
   }
