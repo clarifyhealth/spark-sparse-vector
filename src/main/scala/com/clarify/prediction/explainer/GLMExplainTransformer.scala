2d2aad1fb07bb28fc3ba1a186c03cb7696a656be
alvin <alvin@clarifyhealth.com>
2019-12-18 15:04:39 -0800

    fix the powerHalfLink

--- src/main/scala/com/clarify/prediction/explainer/GLMExplainTransformer.scala
+++ src/main/scala/com/clarify/prediction/explainer/GLMExplainTransformer.scala
@@ -310,8 +310,7 @@ class GLMExplainTransformer(override val uid: String)
       (family, linkFunctionType, linkPower, variancePower) match {
         case ("tweedie", _, 0.0, _) => logLink(family, x, variancePower)
         case ("tweedie", _, 1.0, _) => identityLink(family, x, variancePower)
-        case ("tweedie", _, 0.5, _) =>
-          otherPowerLink(family, x, 2, variancePower)
+
         case ("tweedie", _, -1.0, _)     => inverseLink(family, x, variancePower)
         case ("tweedie", _, y, _)        => otherPowerLink(family, x, y, variancePower)
         case (_, "logLink", _, _)        => logLink(family, x, -1.0)
