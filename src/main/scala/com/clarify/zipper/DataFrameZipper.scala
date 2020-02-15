d8923032dbf802294f699b3c781afc51803f5973
Imran Qureshi <imranq2@hotmail.com>
2020-02-14 20:57:05 -0800

    add zipDataFramesListMultipleColumnsRange

--- src/main/scala/com/clarify/zipper/DataFrameZipper.scala
+++ src/main/scala/com/clarify/zipper/DataFrameZipper.scala
@@ -1,10 +1,11 @@
 package com.clarify.zipper
 
 import java.util
-import scala.collection.JavaConversions._
 
+import scala.collection.JavaConversions._
 import com.clarify.Helpers
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
 object DataFrameZipper {
@@ -69,25 +70,24 @@ object DataFrameZipper {
                                        chunk_number: Int, total_chunks: Int): Boolean = {
 
     val a: DataFrame = sql_ctx.table(view1)
-    val len = views.size()
+    val len: Int = views.size()
     var i: Int = 0
-    var left_rdd = a.rdd
-    var left_schema_fields = a.schema.fields
-    for (view_to_zip <- Helpers.getSeqString(views)) {
+    var left_rdd: RDD[Row] = a.rdd
+    var left_schema_fields: Array[StructField] = a.schema.fields
+    for (view_to_zip: String <- Helpers.getSeqString(views)) {
       i = i + 1
       val b: DataFrame = sql_ctx.table(view_to_zip)
       Helpers.log(f"Zipping data frame $i of $len (chunk $chunk_number of $total_chunks): $view1 <- $view_to_zip, columns=$column_indexes")
-      val right_rdd = b.rdd
+      val right_rdd: RDD[Row] = b.rdd
       column_indexes.foreach {
         column_index: Int =>
-          if (column_index < b.columns.length)
-            {
-              left_rdd = left_rdd.zip(right_rdd).map {
-                case (rowLeft, rowRight) => Row.fromSeq(rowLeft.toSeq :+ rowRight(column_index))
-              }
-
-              left_schema_fields = left_schema_fields :+ b.schema.fields(column_index)
+          if (column_index < b.columns.length) {
+            left_rdd = left_rdd.zip(right_rdd).map {
+              case (rowLeft: Row, rowRight: Row) => Row.fromSeq(rowLeft.toSeq :+ rowRight(column_index))
             }
+
+            left_schema_fields = left_schema_fields :+ b.schema.fields(column_index)
+          }
       }
       true
     }
@@ -99,6 +99,39 @@ object DataFrameZipper {
     Helpers.log(f"Returning result into $result_view")
 
     true
+  }
+
+  def zipDataFramesListMultipleColumnsRange(sql_ctx: SQLContext, view1: String, views: util.ArrayList[String],
+                                            result_view: String, begin_column_index: Int, end_column_index: Int,
+                                            chunk_number: Int, total_chunks: Int): Boolean = {
+
+    val a: DataFrame = sql_ctx.table(view1)
+    val len: Int = views.size()
+    var i: Int = 0
+    var left_rdd: RDD[Row] = a.rdd
+    var left_schema_fields: Array[StructField] = a.schema.fields
+    for (view_to_zip <- Helpers.getSeqString(views)) {
+      i = i + 1
+      val b: DataFrame = sql_ctx.table(view_to_zip)
+      Helpers.log(f"Zipping data frame $i of $len (chunk $chunk_number of $total_chunks): $view1 <- $view_to_zip, begin_column_index=$begin_column_index, end_column_index=$end_column_index")
+      val right_rdd: RDD[Row] = b.rdd
+      val safe_end_column_index = if (end_column_index > b.columns.length - 1) b.columns.length - 1 else end_column_index
+//      Helpers.log(f"zip begin_column_index=$begin_column_index to safe_end_column_index=$safe_end_column_index")
+      left_rdd = left_rdd.zip(right_rdd).map {
+        case (rowLeft: Row, rowRight: Row) => Row.fromSeq(rowLeft.toSeq ++ rowRight.toSeq.slice(begin_column_index, safe_end_column_index + 1))
+      }
+
+      left_schema_fields = left_schema_fields ++ b.schema.fields.slice(begin_column_index, safe_end_column_index + 1)
+      true
+    }
+    val result_df: DataFrame = a.sqlContext.createDataFrame(left_rdd, StructType(left_schema_fields))
+
+    Helpers.log(f"Finished zipping data frames: $view1 with $views")
+    result_df.createOrReplaceTempView(result_view)
+    Helpers.log(f"Returning result into $result_view")
+
+    true
+
   }
 
   def _zipDataFramesSingleColumn(a: DataFrame, b: DataFrame, column_index: Int): DataFrame = {
--- src/test/scala/com/clarify/zipper/DataFrameZipperTest.scala
+++ src/test/scala/com/clarify/zipper/DataFrameZipperTest.scala
@@ -191,5 +191,73 @@ class DataFrameZipperTest extends QueryTest with SparkSessionTestWrapper {
     )
     TestHelpers.clear_tables(spark_session = spark)
   }
+  test("zipDataFramesListMultipleColumnsRange") {
+    spark.sharedState.cacheManager.clearCache()
+
+    val data1 = List(
+      Row(1, "foo", "2foo"),
+      Row(2, "bar", "2bar"),
+      Row(3, "zoo", "2zoo")
+    )
+    val fields1 = List(
+      StructField("id", IntegerType, nullable = false),
+      StructField("v2", StringType, nullable = false),
+      StructField("v21", StringType, nullable = false))
+    val data_rdd1 = spark.sparkContext.makeRDD(data1)
+    val df1: DataFrame = spark.createDataFrame(data_rdd1, StructType(fields1))
+    val table1 = "my_table"
+    df1.createOrReplaceTempView(table1)
+
+    val data2 = List(
+      Row(1, "foo2", "2foo2"),
+      Row(2, "bar2", "2bar2"),
+      Row(3, "zoo2", "2zoo2")
+    )
+    val fields2 = List(
+      StructField("id", IntegerType, nullable = false),
+      StructField("v3", StringType, nullable = false),
+      StructField("v31", StringType, nullable = false))
+    val data_rdd2 = spark.sparkContext.makeRDD(data2)
+    val df2: DataFrame = spark.createDataFrame(data_rdd2, StructType(fields2))
+    val table2 = "my_table2"
+    df2.createOrReplaceTempView(table2)
+
+    val data3 = List(
+      Row(1, "foo3", "2foo3"),
+      Row(2, "bar3", "2bar3"),
+      Row(3, "zoo3", "2zoo3")
+    )
+    val fields3 = List(
+      StructField("id", IntegerType, nullable = false),
+      StructField("v4", StringType, nullable = false),
+      StructField("v41", StringType, nullable = false))
+    val data_rdd3 = spark.sparkContext.makeRDD(data3)
+    val df3: DataFrame = spark.createDataFrame(data_rdd3, StructType(fields3))
+    val table3 = "my_table3"
+    df3.createOrReplaceTempView(table3)
+
+    val views_to_zip = new util.ArrayList[String]()
+    views_to_zip.add(table2)
+    views_to_zip.add(table3)
+
+    //val result_table = "result_table"
+    DataFrameZipper.zipDataFramesListMultipleColumnsRange(df1.sqlContext, table1, views_to_zip, table1, 1,3, 1, 1)
+
+    // now test reading from it
+    val result_df: DataFrame = spark.table(table1)
+    result_df.show()
+
+    assert(result_df.count() == df1.count())
+
+    checkAnswer(
+      result_df,
+      Seq(
+        Row(1, "foo", "2foo", "foo2", "2foo2", "foo3", "2foo3"),
+        Row(2, "bar", "2bar", "bar2", "2bar2", "bar3", "2bar3"),
+        Row(3, "zoo", "2zoo", "zoo2", "2zoo2", "zoo3", "2zoo3")
+      )
+    )
+    TestHelpers.clear_tables(spark_session = spark)
+  }
 }
 
