a1537cf6f8729a0cb02e73a2f2d57b6885bcf3f1
Imran Qureshi <imranq2@hotmail.com>
2020-02-20 10:04:41 -0800

    add test for DenseVector

--- src/test/scala/com/clarify/sparse_vectors/StructToVectorTest.scala
+++ src/test/scala/com/clarify/sparse_vectors/StructToVectorTest.scala
@@ -1,11 +1,10 @@
 package com.clarify.sparse_vectors
 
-import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
-import org.apache.spark.sql.types.{ArrayType, ByteType, DoubleType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.struct
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, QueryTest, Row}
-import org.apache.spark.sql.functions.{col, struct, udf}
 
 
 class StructToVectorTest extends QueryTest with SparkSessionTestWrapper {
@@ -23,7 +22,7 @@ class StructToVectorTest extends QueryTest with SparkSessionTestWrapper {
     val v3 = new StructToVector().convert_struct_to_vector(v1)
     assert(v3 == new SparseVector(3, Array(0, 1, 2), Array(3.0, 4.0, 5.0)))
   }
-  test("struct to vector data frame") {
+  test("struct to vector data frame sparse vector") {
     spark.sharedState.cacheManager.clearCache()
 
     val rdd = spark.sparkContext.parallelize(
@@ -59,6 +58,46 @@ class StructToVectorTest extends QueryTest with SparkSessionTestWrapper {
       Seq(
         Row(new SparseVector(2, Array(0, 1), Array(1.0, 2.0))),
         Row(new SparseVector(3, Array(0, 1, 2), Array(3.0, 4.0, 5.0)))
+      )
+    )
+    assert(2 == out_df.count())
+  }
+  test("struct to vector data frame sparse and dense vectors") {
+    spark.sharedState.cacheManager.clearCache()
+
+    val rdd = spark.sparkContext.parallelize(
+      Row(0.toByte, 2, List(0, 1).toArray, List(1.0, 2.0).toArray) ::
+        Row(1.toByte, 0, Nil, List(3.0, 4.0, 5.0).toArray) ::
+        Nil)
+
+    val schema = StructType(
+      StructField("type", ByteType, false) ::
+        StructField("size", IntegerType, false) ::
+        StructField("indices", ArrayType(IntegerType, false), false) ::
+        StructField("values", ArrayType(DoubleType, false), false) ::
+        Nil)
+
+    val df: DataFrame = spark.createDataFrame(rdd, schema).select(struct("type", "size", "indices", "values").alias("v1"))
+
+    df.printSchema()
+    df.show()
+    df.createOrReplaceTempView("my_table2")
+    val convert_struct_to_vector_function = new StructToVector().call _
+
+    spark.udf.register("convert_struct_to_vector", convert_struct_to_vector_function)
+
+    val out_df = spark.sql(
+      "select convert_struct_to_vector(v1) as result from my_table2"
+    )
+
+    out_df.show(truncate = false)
+    out_df.printSchema()
+
+    checkAnswer(
+      out_df.selectExpr("result"),
+      Seq(
+        Row(new SparseVector(2, Array(0, 1), Array(1.0, 2.0))),
+        Row(new DenseVector(Array(3.0, 4.0, 5.0)))
       )
     )
     assert(2 == out_df.count())
