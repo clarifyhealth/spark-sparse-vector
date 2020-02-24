35bd9977687ceccb17fa0d7b3d4b069ac9ff8826
alvin <alvin@clarifyhealth.com>
2020-02-23 17:28:05 -0800

    fix typo

--- src/main/scala/com/clarify/model/meta/ModelMetaTransformer.scala
+++ src/main/scala/com/clarify/model/meta/ModelMetaTransformer.scala
@@ -86,7 +86,7 @@ class ModelMetaTransformer(override val uid: String)
     new Param[String](
       this,
       "labelCol",
-      "training label name" n
+      "training label name"
     )
 
   final def getLabelCol: String = $(labelCol)
