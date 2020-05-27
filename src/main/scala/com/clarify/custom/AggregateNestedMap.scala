0d99a467f2988adda444162501c64cd3cc96203f
alvin <alvin@clarifyhealth.com>
2020-05-27 14:45:10 -0700

    check for all non nulls in struct

--- src/main/scala/com/clarify/custom/AggregateNestedMap.scala
+++ src/main/scala/com/clarify/custom/AggregateNestedMap.scala
@@ -1,15 +1,25 @@
 package com.clarify.custom
 
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.api.java.{UDF1}
+import org.apache.spark.sql.api.java.UDF1
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 
-class AggregateNestedMap extends UDF1[Seq[Map[String, Row]], Map[String, Row]] {
+class AggregateNestedMap
+    extends UDF1[Seq[Map[String, GenericRowWithSchema]], Map[String, Row]] {
 
   override def call(
-      entries: Seq[Map[String, Row]]
+      entries: Seq[Map[String, GenericRowWithSchema]]
   ): Map[String, Row] = {
     entries.flatMap { item =>
-      item.filter { case (_, value: Row) => !value.isNullAt(0) }
+      item.filter {
+        case (_, value: GenericRowWithSchema) =>
+          if (value.schema.names.contains("predicted_value") || value.schema.names
+                .contains("sigma")) {
+            !value.anyNull
+          } else {
+            !value.isNullAt(0)
+          }
+      }
     }.toMap
   }
 }
