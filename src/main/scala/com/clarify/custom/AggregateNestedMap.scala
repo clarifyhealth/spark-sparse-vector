b586735307d6705d4859ddca66e54ff6ad3c8cea
alvin <alvin@clarifyhealth.com>
2020-05-27 20:38:43 -0700

    roll back to original check

--- src/main/scala/com/clarify/custom/AggregateNestedMap.scala
+++ src/main/scala/com/clarify/custom/AggregateNestedMap.scala
@@ -2,18 +2,14 @@ package com.clarify.custom
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.api.java.UDF1
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 
-class AggregateNestedMap
-    extends UDF1[Seq[Map[String, GenericRowWithSchema]], Map[String, Row]] {
+class AggregateNestedMap extends UDF1[Seq[Map[String, Row]], Map[String, Row]] {
 
   override def call(
-      entries: Seq[Map[String, GenericRowWithSchema]]
+      entries: Seq[Map[String, Row]]
   ): Map[String, Row] = {
     entries.flatMap { item =>
-      item.filter {
-        case (_, value: GenericRowWithSchema) => !value.isNullAt(0)
-      }
+      item.filter { case (_, value: Row) => !value.isNullAt(0) }
     }.toMap
   }
 }
