b0f4bba26bfcb7ab96fad95fb698f8818d2bdc45
alvin <alvin@clarifyhealth.com>
2020-06-03 08:22:51 -0700

    update row Value chck

--- src/main/scala/com/clarify/custom/AggregateNestedMap.scala
+++ src/main/scala/com/clarify/custom/AggregateNestedMap.scala
@@ -2,14 +2,26 @@ package com.clarify.custom
 
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.api.java.UDF1
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 
-class AggregateNestedMap extends UDF1[Seq[Map[String, Row]], Map[String, Row]] {
+class AggregateNestedMap
+    extends UDF1[Seq[Map[String, GenericRowWithSchema]], Map[String, Row]] {
 
   override def call(
-      entries: Seq[Map[String, Row]]
+      entries: Seq[Map[String, GenericRowWithSchema]]
   ): Map[String, Row] = {
     entries.flatMap { item =>
-      item.filter { case (_, value: Row) => !value.isNullAt(0) }
+      item.filter {
+        case (_, rowValue: GenericRowWithSchema) =>
+          if (rowValue.schema.names.contains("predicted_value")) {
+            !rowValue.isNullAt(rowValue.fieldIndex("predicted_value"))
+          } else if (rowValue.schema.names.contains("contrib_intercept")) {
+            !rowValue.isNullAt(rowValue.fieldIndex("contrib_intercept"))
+          } else {
+            !rowValue.anyNull
+          }
+        case (_, value: GenericRowWithSchema) => !value.isNullAt(0)
+      }
     }.toMap
   }
 }
