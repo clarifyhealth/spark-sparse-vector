f83f764ddbcef16ee0131dfbdd800bd5abb6317d
alvin <alvin@clarifyhealth.com>
2020-07-07 15:59:41 -0700

    match to numpy

--- src/main/scala/com/clarify/array/StdDev.scala
+++ src/main/scala/com/clarify/array/StdDev.scala
@@ -5,6 +5,6 @@ import org.apache.spark.sql.api.java.UDF1
 
 class StdDev extends UDF1[Seq[Double], Double] {
   override def call(data: Seq[Double]): Double = {
-    stddev(data)
+    stddev(data) * Math.pow((data.length - 1) / data.length, .5)
   }
 }
