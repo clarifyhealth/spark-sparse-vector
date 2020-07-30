a99ab941eac37cd89aacda9fadce8d998374d266
alvin <alvin@clarifyhealth.com>
2020-07-29 19:30:19 -0700

    fix optional

--- src/main/scala/com/clarify/disruption/v2/Calculator.scala
+++ src/main/scala/com/clarify/disruption/v2/Calculator.scala
@@ -2,15 +2,16 @@ package com.clarify.disruption.v2
 
 import org.apache.spark.sql.api.java.UDF1
 
+import scala.util.Try
+
 class Calculator extends UDF1[Seq[Double], Option[Double]] {
   override def call(data: Seq[Double]): Option[Double] = {
 
     val diff = for (i <- 1 until data.length)
       yield data(i) - data(i - 1)
 
-    val index = diff.indices.maxBy(x => math.abs(x))
+    val index = Try(diff.indices.maxBy(x => math.abs(x))).toOption
 
-    diff.lift(index)
-
+    diff.lift(index.getOrElse(-1))
   }
 }
