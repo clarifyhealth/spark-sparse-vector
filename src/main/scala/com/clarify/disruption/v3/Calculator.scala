0a75d8bd9b0bd2238d2f1dcfd296a2b0c2ac842f
Imran Qureshi <imranq2@hotmail.com>
2020-08-12 14:57:03 -0700

    merge changes from master

--- src/main/scala/com/clarify/disruption/v3/Calculator.scala
+++ src/main/scala/com/clarify/disruption/v3/Calculator.scala
@@ -7,38 +7,30 @@ import scala.util.Try
 class Calculator extends UDF1[Seq[Double], Option[Double]] {
   override def call(data: Seq[Double]): Option[Double] = {
 
-    val data_sanitized = data.map {
-      case d if(d.isNaN) => 0.0 // Or whatever value you'd prefer.
-      case d if(d.isNegInfinity) => 0.0 // Or whatever value you'd prefer.
-      case d if(d.isPosInfinity) => 0.0 // Or whatever value you'd prefer.
-      case d => d
-    }
-
-    // calculate the delta for each reading (subtracting the previous entry from it)
-    val diff: IndexedSeq[Double] = {
-      for (i <- 1 until data_sanitized.length)
-        yield data_sanitized(i) - data_sanitized(i - 1)
-    }
     Option(data) match {
       case Some(value: Seq[Double]) =>
-        if (value.exists(_.isNaN)) {
-          Some(0.0)
+        val data_sanitized = data.map {
+          case d if d.isNaN => 0.0 // Or whatever value you'd prefer.
+          case d if d.isNegInfinity => 0.0 // Or whatever value you'd prefer.
+          case d if d.isPosInfinity => 0.0 // Or whatever value you'd prefer.
+          case d => d
         }
-        else {
-          // calculate the delta for each reading (subtracting the previous entry from it)
-          val diff: IndexedSeq[Double] = for (i <- 1 until value.length)
-            yield value(i) - value(i - 1)
 
-          // find index of element with the minimum value
-          //    val index = Try(diff.indices.minBy(x => x)).toOption
-          val index: Option[Int] = Try(diff.indices.minBy(diff)).toOption
-
-          // use value or -1 if there is no value
-          val out: Option[Double] = diff.lift(index.getOrElse(-1))
-
-          // change any value greater than 0 to 0 so we get only negative disruption
-          Some(Array(out.getOrElse(0.0), 0.0).min)
+        // calculate the delta for each reading (subtracting the previous entry from it)
+        val diff: IndexedSeq[Double] = {
+          for (i <- 1 until data_sanitized.length)
+            yield data_sanitized(i) - data_sanitized(i - 1)
         }
+
+        // find index of element with the minimum value
+        //    val index = Try(diff.indices.minBy(x => x)).toOption
+        val index: Option[Int] = Try(diff.indices.minBy(diff)).toOption
+
+        // use value or -1 if there is no value
+        val out: Option[Double] = diff.lift(index.getOrElse(-1))
+
+        // change any value greater than 0 to 0 so we get only negative disruption
+        Some(Array(out.getOrElse(0.0), 0.0).min)
       case None =>
         Some(0.0)
     }
