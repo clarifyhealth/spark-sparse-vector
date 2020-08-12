e8decad96f34a02820986e266b5d9acf166e4c6d
Imran Qureshi <imranq2@hotmail.com>
2020-08-12 14:53:48 -0700

    add one more null test

--- src/main/scala/com/clarify/disruption/v3/Calculator.scala
+++ src/main/scala/com/clarify/disruption/v3/Calculator.scala
@@ -7,19 +7,28 @@ import scala.util.Try
 class Calculator extends UDF1[Seq[Double], Option[Double]] {
   override def call(data: Seq[Double]): Option[Double] = {
 
-    // calculate the delta for each reading (subtracting the previous entry from it)
-    val diff: IndexedSeq[Double] = for (i <- 1 until data.length)
-      yield data(i) - data(i - 1)
+    Option(data) match {
+      case Some(value: Seq[Double]) =>
+        if (value.exists(_.isNaN)) {
+          Some(0.0)
+        }
+        else {
+          // calculate the delta for each reading (subtracting the previous entry from it)
+          val diff: IndexedSeq[Double] = for (i <- 1 until value.length)
+            yield value(i) - value(i - 1)
 
-    // find index of element with the minimum value
-//    val index = Try(diff.indices.minBy(x => x)).toOption
-    val index: Option[Int] = Try(diff.indices.minBy(diff)).toOption
+          // find index of element with the minimum value
+          //    val index = Try(diff.indices.minBy(x => x)).toOption
+          val index: Option[Int] = Try(diff.indices.minBy(diff)).toOption
 
-    // use value or -1 if there is no value
-    val out: Option[Double] = diff.lift(index.getOrElse(-1))
+          // use value or -1 if there is no value
+          val out: Option[Double] = diff.lift(index.getOrElse(-1))
 
-    // change any value greater than 0 to 0 so we get only negative disruption
-    Some(Array(out.getOrElse(0.0), 0.0).min)
-
+          // change any value greater than 0 to 0 so we get only negative disruption
+          Some(Array(out.getOrElse(0.0), 0.0).min)
+        }
+      case None =>
+        Some(0.0)
+    }
   }
 }
